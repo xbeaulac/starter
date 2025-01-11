d0a2fe66e25f9f9b2abae20ea2e9e8d482b18a1c
Xander Beaulac <xbeaulac@gmail.com>
2025-01-11 09:14:25 -0800

    created people table and its rls policies

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,30 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgPolicy, pgTable, text } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { authenticatedRole } from "drizzle-orm/supabase";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+export const peopleTable = pgTable(
+  "people",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: text().notNull(),
+    age: integer().notNull(),
+    height: integer().notNull(),
+    user_id: text()
+      .notNull()
+      .default(sql`requesting_user_id()`),
+  },
+  (table) => [
+    pgPolicy("Enable read access for authenticated users only", {
+      as: "permissive",
+      to: authenticatedRole,
+      for: "select",
+      using: sql`requesting_user_id() = user_id`,
+    }),
+    pgPolicy("Enable insert for authenticated users only", {
+      as: "permissive",
+      to: authenticatedRole,
+      for: "insert",
+      withCheck: sql`requesting_user_id() = user_id`,
+    }),
+  ],
+);
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: "./db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_SESSION_URL!,
+    url: process.env.DATABASE_URL!,
   },
 });
